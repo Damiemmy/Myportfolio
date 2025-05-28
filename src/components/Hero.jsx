5233e04201e2ac9a036064fd35b80794899e62d9
Damiemmy <damisaemmanuel778@gmail.com>
2025-05-28 16:33:31 +0100

    Feat:added Resume.Pdf

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../static/Hero.css"
 import image from '../assets/web.jpg'
+import PDF from '../assets/Damisa.pdf'
 
 const Hero = () => {
   return (
@@ -10,7 +11,7 @@ const Hero = () => {
         <p>I am a Frontend Developer From Abuja,Nigeria with 3 years of Experience,i build Landing Pages and redesign Figma Designs to a react Components </p>
         <div className='hero-action'>
             <div className='hero-connect'>Connect with Me</div>
-            <div className='hero-myresume'>My resume</div>
+            <div className='hero-myresume'> <a href={PDF} download>my resume</a></div>
         </div>
     </div>
   )
